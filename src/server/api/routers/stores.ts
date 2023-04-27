7d1f473fd04f37e0072533c47e5a6ec211d53b85
Rogelio Camargo <rogelio.camargo2016@gmail.com>
2023-04-26 21:31:03 -0700

    Write create mutation for stores

--- src/server/api/routers/stores.ts
+++ src/server/api/routers/stores.ts
@@ -1,9 +1,32 @@
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, privateProcedure } from "~/server/api/trpc";
 
 export const storesRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.store.findMany();
+  getAll: privateProcedure.query(({ ctx }) => {
+    return ctx.prisma.store.findMany({
+      where: {
+        userId: ctx.currentUserId,
+      },
+    });
   }),
+
+  create: privateProcedure
+    .input(
+      z.object({
+        storeNumber: z.number({ required_error: "Store number is required" }),
+        name: z.string({ required_error: "Name is required" }),
+        location: z.string({ required_error: "Location is required" }),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const store = await ctx.prisma.store.create({
+        data: {
+          ...input,
+          userId: ctx.currentUserId,
+        },
+      });
+
+      return store;
+    }),
 });
